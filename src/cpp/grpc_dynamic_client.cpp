d7c8096a443c51c15763a71e7fbbc4fe7df8e0e6
Sean Moore <sean.moore@ni.com>
2022-01-26 13:20:48 -0600

    simplify

--- src/cpp/grpc_dynamic_client.cpp
+++ src/cpp/grpc_dynamic_client.cpp
@@ -6,14 +6,10 @@
 #include <google/protobuf/dynamic_message.h>
 #include <google/protobuf/util/json_util.h>
 #include <grpc/grpc.h>
-#include <grpcpp/generic/generic_stub.h>
+#include <grpcpp/impl/client_unary_call.h>
 
 #include "grpc_dynamic_client.h"
 #include "proto_reflection_descriptor_database.h"
-
-// TODO(sean): remove after solving blocking unary call issue
-#include <thread>
-#include <chrono>
 
 using namespace std;
 
@@ -35,28 +31,16 @@ int main()
 	google::protobuf::Message* response = response_prototype->New();
 	google::protobuf::util::JsonStringToMessage("{\"resource_name\": \"VST2_01\"}", request);
 
-	grpc::ByteBuffer serialized_request;
-	bool own_buffer = false;
-	grpc::GenericSerialize<grpc::ProtoBufferWriter, google::protobuf::Message>(*request, &serialized_request, &own_buffer);
-	
-	grpc::ByteBuffer serialized_response;
-	grpc::GenericStub stub(channel);
-	function<void(grpc::Status)> on_complete = [](grpc::Status) { cout << "call complete" << endl; };
-	stub.UnaryCall(&grpc::ClientContext(), "/nirfsa_grpc.NiRFSA/Init", grpc::StubOptions(), &serialized_request, &serialized_response, on_complete);
+	grpc::internal::RpcMethod method("/nirfsa_grpc.NiRFSA/Init", grpc::internal::RpcMethod::NORMAL_RPC);
+	grpc::internal::BlockingUnaryCall(channel.get(), method, &grpc::ClientContext(), *request, response);
 
-	// TODO(sean): figure out how to wait for the call to complete instead of a magic delay fairy
-	// if we don't wait for the call to complete the response isn't populated and the deserialization returns the default message
-	this_thread::sleep_for(chrono::milliseconds(500));
-
-	grpc::GenericDeserialize<grpc::ProtoBufferReader, google::protobuf::Message>(&serialized_response, response);
 	string response_json;
 	google::protobuf::util::JsonOptions json_options;
-	json_options.add_whitespace = true;
 	json_options.always_print_primitive_fields = true;
 	json_options.preserve_proto_field_names = true;
 	google::protobuf::util::MessageToJsonString(*response, &response_json, json_options);
 
 	cout << response_json << endl;
-	
+
 	return 0;
 }
