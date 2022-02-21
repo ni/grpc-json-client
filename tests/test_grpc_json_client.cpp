c1a2bf435274e376f57cf5884f2c50963597d3ac
Sean Moore <sean.moore@ni.com>
2022-02-21 11:29:51 -0600

    fix tests

--- tests/test_grpc_json_client.cpp
+++ tests/test_grpc_json_client.cpp
@@ -53,13 +53,14 @@ namespace ni
         TEST_F(GrpcJsonClientTest, WriteSucceedsOnValidWrite)
         {
             json request = { {"session_name", "gtest"}, {"resource_name", "VST2_01"} };
-            ASSERT_EQ(Write(session, "nirfsa_grpc.NiRFSA", "Init", request.dump().c_str()), 0);
+            void* tag1;
+            ASSERT_EQ(StartAsyncCall(session, "nirfsa_grpc.NiRFSA", "Init", request.dump().c_str(), &tag1), 0);
 
             int timeout = 10000;
             size_t size = 0;
-            ASSERT_EQ(Read(session, timeout, nullptr, &size), 0);
+            ASSERT_EQ(FinishAsyncCall(session, tag1, timeout, nullptr, &size), 0);
             unique_ptr<char> buffer(new char[size]);
-            ASSERT_EQ(Read(session, timeout, buffer.get(), &size), 0);
+            ASSERT_EQ(FinishAsyncCall(session, tag1, timeout, buffer.get(), &size), 0);
             json response = json::parse(buffer.get());
             ASSERT_EQ(response["status"], 0);
             json vi = response["vi"];
@@ -67,22 +68,23 @@ namespace ni
             request.clear();
             request["vi"] = vi;
             request["attribute_id"] = "NIRFSA_ATTRIBUTE_REFERENCE_LEVEL";
-            request["value_raw"] = 40.0;
-            ASSERT_EQ(Write(session, "nirfsa_grpc.NiRFSA", "SetAttributeViReal64", request.dump().c_str()), 0);
+            request["value_raw"] = -10.0;
+            ASSERT_EQ(StartAsyncCall(session, "nirfsa_grpc.NiRFSA", "SetAttributeViReal64", request.dump().c_str(), &tag1), 0);
             
             request.clear();
             request["vi"] = vi;
-            ASSERT_EQ(Write(session, "nirfsa_grpc.NiRFSA", "Commit", request.dump().c_str()), 0);
+            void* tag2;
+            ASSERT_EQ(StartAsyncCall(session, "nirfsa_grpc.NiRFSA", "Commit", request.dump().c_str(), &tag2), 0);
 
-            ASSERT_EQ(Read(session, timeout, nullptr, &size), 0);
+            ASSERT_EQ(FinishAsyncCall(session, tag1, timeout, nullptr, &size), 0);
             buffer = unique_ptr<char>(new char[size]);
-            ASSERT_EQ(Read(session, timeout, buffer.get(), &size), 0);
+            ASSERT_EQ(FinishAsyncCall(session, tag1, timeout, buffer.get(), &size), 0);
             response = json::parse(buffer.get());
             ASSERT_EQ(response["status"], 0);
 
-            ASSERT_EQ(Read(session, timeout, nullptr, &size), 0);
+            ASSERT_EQ(FinishAsyncCall(session, tag2, timeout, nullptr, &size), 0);
             buffer = unique_ptr<char>(new char[size]);
-            ASSERT_EQ(Read(session, timeout, buffer.get(), &size), 0);
+            ASSERT_EQ(FinishAsyncCall(session, tag2, timeout, buffer.get(), &size), 0);
             response = json::parse(buffer.get());
             ASSERT_EQ(response["status"], 0);
         }
