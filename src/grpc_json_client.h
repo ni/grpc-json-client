ee284a3ceef06481905447a8fb0f1d417c594ee2
Sean Moore <sean.moore@ni.com>
2022-02-21 14:56:12 -0600

    prefix exports

--- src/grpc_json_client.h
+++ src/grpc_json_client.h
@@ -8,25 +8,25 @@ extern "C"
 #endif
 
 // Initialize a session with an insecure channel.
-__declspec(dllexport) int32_t InitInsecure(const char* target, void** session_handle);
+__declspec(dllexport) int32_t GrpcJsonClient_InitInsecure(const char* target, void** session_handle);
 
 // Start a call to the host.
-__declspec(dllexport) int32_t StartAsyncCall(void* session_handle, const char* service, const char* method, const char* request, void** tag);
+__declspec(dllexport) int32_t GrpcJsonClient_StartAsyncCall(void* session_handle, const char* service, const char* method, const char* request, void** tag);
 
 // Finish a call to the host.
-__declspec(dllexport) int32_t FinishAsyncCall(void* session_handle, void* tag, int32_t timeout, char* response, size_t* size);
+__declspec(dllexport) int32_t GrpcJsonClient_FinishAsyncCall(void* session_handle, void* tag, int32_t timeout, char* response, size_t* size);
 
 // Lock the session.
-__declspec(dllexport) int32_t LockSession(void* session_handle);
+__declspec(dllexport) int32_t GrpcJsonClient_LockSession(void* session_handle);
 
 // Unlock the session.
-__declspec(dllexport) int32_t UnlockSession(void* session_handle);
+__declspec(dllexport) int32_t GrpcJsonClient_UnlockSession(void* session_handle);
 
 // Release resources allocated by the session.
-__declspec(dllexport) int32_t Close(void* session_handle);
+__declspec(dllexport) int32_t GrpcJsonClient_Close(void* session_handle);
 
 // Get error information.
-__declspec(dllexport) int32_t GetError(void* session_handle, int32_t* code, char* buffer, size_t* size);
+__declspec(dllexport) int32_t GrpcJsonClient_GetError(void* session_handle, int32_t* code, char* buffer, size_t* size);
 
 #ifdef __cplusplus
 }
