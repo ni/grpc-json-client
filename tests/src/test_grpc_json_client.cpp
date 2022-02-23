4cd53ba5eaa2e954b13968d4b579a577106530f3
Sean Moore <sean.moore@ni.com>
2022-02-23 16:59:59 -0600

    use timeouts

--- tests/src/test_grpc_json_client.cpp
+++ tests/src/test_grpc_json_client.cpp
@@ -64,9 +64,9 @@ namespace ni
                 void* tag = nullptr;
                 ASSERT_EQ(GrpcJsonClient_StartAsyncCall(session, "ni.grpc_json_client.TestingService", "UnaryUnaryEcho", request.dump().c_str(), &tag), 0);
                 size_t size = 0;
-                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tag, -1, nullptr, &size), 0);
+                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tag, 100, nullptr, &size), 0);
                 char* buffer = new char[size];
-                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tag, -1, buffer, &size), 0);
+                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tag, 100, buffer, &size), 0);
                 json response = json::parse(buffer);
                 delete[] buffer;
                 ASSERT_EQ(response["string_field"], string_field);
@@ -87,9 +87,9 @@ namespace ni
             for (size_t i = 0; i < 2; i++)
             {
                 size_t size = 0;
-                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tags[i], 1000, nullptr, &size), 0);
+                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tags[i], 100, nullptr, &size), 0);
                 char* buffer = new char[size];
-                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tags[i], 1000, buffer, &size), 0);
+                ASSERT_EQ(GrpcJsonClient_FinishAsyncCall(session, tags[i], 100, buffer, &size), 0);
                 json response = json::parse(buffer);
                 delete[] buffer;
                 ASSERT_EQ(response["string_field"], string_fields[i]);
