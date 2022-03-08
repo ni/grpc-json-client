0cca7e39a58f834f7fb92efdd0ffa9c6f5e6c169
Sean Moore <sean.moore@ni.com>
2022-03-08 13:07:07 -0600

    use dllexport macro

--- src/grpc_json_client.h
+++ src/grpc_json_client.h
@@ -3,23 +3,23 @@
 
 #include <stdint.h>
 
+#define DLLEXPORT __declspec(dllexport)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 // Initializes a session with an insecure channel.
-__declspec(dllexport) int32_t GrpcJsonClient_Initialize(
-    const char* target, intptr_t* session_handle);
+DLLEXPORT int32_t GrpcJsonClient_Initialize(const char* target, intptr_t* session_handle);
 
 // Resets the descriptor database to it's default state.
-__declspec(dllexport) int32_t GrpcJsonClient_ResetDescriptorDatabase(intptr_t session_handle);
+DLLEXPORT int32_t GrpcJsonClient_ResetDescriptorDatabase(intptr_t session_handle);
 
 // Queries the reflection service for file descriptors.
-__declspec(dllexport) int32_t GrpcJsonClient_FillDescriptorDatabase(
-    intptr_t session_handle, int32_t timeout);
+DLLEXPORT int32_t GrpcJsonClient_FillDescriptorDatabase(intptr_t session_handle, int32_t timeout);
 
 // Starts an asynchronous call.
-__declspec(dllexport) int32_t GrpcJsonClient_StartAsyncCall(
+DLLEXPORT int32_t GrpcJsonClient_StartAsyncCall(
     intptr_t session_handle,
     const char* service,
     const char* method,
@@ -28,11 +28,11 @@ __declspec(dllexport) int32_t GrpcJsonClient_StartAsyncCall(
     intptr_t* tag);
 
 // Finishes an asynchronous call.
-__declspec(dllexport) int32_t GrpcJsonClient_FinishAsyncCall(
+DLLEXPORT int32_t GrpcJsonClient_FinishAsyncCall(
     intptr_t session_handle, intptr_t tag, int32_t timeout, char* response, size_t* size);
 
 // Performs a blocking call.
-__declspec(dllexport) int32_t GrpcJsonClient_BlockingCall(
+DLLEXPORT int32_t GrpcJsonClient_BlockingCall(
     intptr_t session_handle,
     const char* service,
     const char* method,
@@ -43,21 +43,21 @@ __declspec(dllexport) int32_t GrpcJsonClient_BlockingCall(
     size_t* size);
 
 // Locks the session.
-__declspec(dllexport) int32_t GrpcJsonClient_LockSession(intptr_t session_handle);
+DLLEXPORT int32_t GrpcJsonClient_LockSession(intptr_t session_handle);
 
 // Unlocks the session.
-__declspec(dllexport) int32_t GrpcJsonClient_UnlockSession(intptr_t session_handle);
+DLLEXPORT int32_t GrpcJsonClient_UnlockSession(intptr_t session_handle);
 
 // Gets error information.
-__declspec(dllexport) int32_t GrpcJsonClient_GetError(
+DLLEXPORT int32_t GrpcJsonClient_GetError(
     intptr_t session_handle, int32_t* code, char* buffer, size_t* size);
 
 // Gets error information.
-__declspec(dllexport) int32_t GrpcJsonClient_GetErrorString(
+DLLEXPORT int32_t GrpcJsonClient_GetErrorString(
     intptr_t session_handle, int32_t code, char* buffer, size_t* size);
 
 // Releases resources allocated by the session.
-__declspec(dllexport) int32_t GrpcJsonClient_Close(intptr_t session_handle);
+DLLEXPORT int32_t GrpcJsonClient_Close(intptr_t session_handle);
 
 #ifdef __cplusplus
 }
