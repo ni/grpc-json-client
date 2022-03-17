c5aaac2232f8fb12c7155998731a471dff23ace8
Sean Moore <sean.moore@ni.com>
2022-03-17 16:15:09 -0500

    use windows handlers

--- tests/src/standalone_testing_server.cpp
+++ tests/src/standalone_testing_server.cpp
@@ -3,18 +3,20 @@
 
 #include "testing_server.h"
 
-using ni::grpc_json_client::TestingServer;
+ni::grpc_json_client::TestingServer server("0.0.0.0:50051");
 
-TestingServer server("0.0.0.0:50051");
-
-void signal_handler(int signal) {
+BOOL WINAPI event_handler(DWORD) {
     server.Stop();
+    server.Wait();
     std::cout << "Server stopped." << std::endl;
+    return TRUE;
 }
 
 int main() {
     server.EnableReflection();
     server.Start();
+    SetConsoleCtrlHandler(event_handler, TRUE);
     std::cout << "Server started on port 50051." << std::endl;
+    std::cout << "Press ctrl+c to stop." << std::endl;
     server.Wait();
 }
